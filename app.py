099f46e2a09aab3031c0eff0968a78735d4903b4
João Gabriel <joaogabrielsantosfernadnes@gmail.com>
2023-08-10 16:33:43 -0300

    [FEAT] Create Endpoint for methods Users

--- app.py
+++ app.py
@@ -1,30 +1,71 @@
-from flask import Flask
-from flask import json
+from flask import Flask, request, json
+from datetime import datetime
+from src.view.views import UserViewSet
+
 
 app = Flask(__name__)
+user_viewset = UserViewSet()
 
-@app.route('/api/v1/user/user_create')
+
+@app.route("/api/v1/user/user_create", methods=["POST"])
 def create_user():
+    data = request.get_json()
+    data["created"] = datetime.today()
+
     response = app.response_class(
-        response=json.dumps("This is an example app test"),
-        status=200,
-        mimetype='application/json'
+        response=json.dumps(
+            {
+                "message": "User created successfully",
+                "inserted_id": user_viewset.create(data),
+                "created_in": datetime.today().strftime("%c"),
+            }
+        ),
+        status=201,
+        mimetype="application/json",
     )
     return response
 
 
-def retrieve_user():
+@app.route("/api/v1/users/", methods=["GET"])
+def list_user():
     response = app.response_class(
-        response=json.dumps("This is an example app test"),
-        status=200,
-        mimetype='application/json'
+        response=user_viewset.find(), status=200, mimetype="application/json"
     )
     return response
 
-def delete_user():
+
+@app.route("/api/v1/user/<int:user_id>/user_retrieve/", methods=["GET"])
+def retrieve_user(user_id):
     response = app.response_class(
-        response=json.dumps("This is an example app test"),
+        response=json.dumps(user_viewset.find_by_id(user_id)),
         status=200,
-        mimetype='application/json'
+        mimetype="application/json",
     )
     return response
+
+
+@app.route("/api/v1/user/<int:user_id>/user_delete", methods=["DELETE"])
+def delete_user(user_id):
+    response = app.response_class(
+        response=json.dumps(user_viewset.delete(user_id)),
+        status=200,
+        mimetype="application/json",
+    )
+    return response
+
+
+# @app.route("/api/v1/usesrs/", methods=["GET"])
+# def update_user():
+#     data = request.get_json()
+#     data["updated"] = datetime.today()
+
+#     response = app.response_class(
+#         response=user_viewset.find({}),
+#         status=201,
+#         mimetype="application/json"
+#     )
+#     return response
+
+
+if __name__ == "__main__":
+    app.run()
